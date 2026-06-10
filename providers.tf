20c06afccacba41d921c8b5f7fa7fb4bd347d83b
Djellza Jasiqi <150348910+DiellzaJ@users.noreply.github.com>
2026-06-10 21:35:58 +0200

    fixed versions and providers

--- providers.tf
+++ providers.tf
@@ -1,15 +1,5 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
 
-}
 
 provider "aws" {
-  region     = var.aws_region
-  access_key = aws_access_key
-  secret_key = aws_secret_key
+  region = "eu-central-1"
 }
